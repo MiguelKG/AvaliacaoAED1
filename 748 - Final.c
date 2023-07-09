b027119506028c2c61f88ebb0e4e9757450ad15d
Miguel <@mail.com>
2023-07-09 03:46:28 -0300

    Código Doom 3

--- 748 - Final.c
+++ 748 - Final.c
@@ -3,36 +3,43 @@
 #include <ctype.h>
 #include <string.h>
 
-char * shortestCompletingWord(char * licensePlate, char ** words, int wordsSize);
+char * ShortestCompletingWord( char * licensePlate, char ** words, int wordsSize );
 
-int main () {
-    char licensePlate[] = "Ah71752";
-    char *words[] = {"suggest","letter","of","husband","easy","education","drug","prevent","writer","old"};
+int main ( ) {
+    char licensePlate[] = "PrL3AA99999l6e";
+    char *words[] = { "parable", "parallelogram", "parallel", "3999996" };
 
-    shortestCompletingWord(licensePlate, words, ( sizeof(words) / sizeof(words[0]) ));
+    ShortestCompletingWord( licensePlate, words, ( sizeof( words ) / sizeof( words[0] ) ) );
 }
 
-char * shortestCompletingWord(char * licensePlate, char ** words, int wordsSize){
+/*
+====================
+ShortestCompletingWord
+ retorna a menor palavra de "words" a ter todos os caracteres de "licensePlate"
+====================
+*/
+
+char * ShortestCompletingWord( char * licensePlate, char ** words, int wordsSize ){
     int c = 0, change = 0, shortSize = -1, shortestWord = 0;
     char wordsCopy[wordsSize][15];
 
-    for (int i = 0; i < wordsSize; i++) {
-        strcpy(wordsCopy[i], words[i]);
+    for ( int i = 0; i < wordsSize; i++ ) {
+        strcpy( wordsCopy[i], words[i] );
     }
 
-    for (int i = 0; i < wordsSize; i++) {
+    for ( int i = 0; i < wordsSize; i++ ) {
         c = 0;
-        for (int j = 0; j < strlen(licensePlate); j++) {
-            if (licensePlate[j] != ' ' && isdigit(licensePlate[j]) == 0) {
+        for ( int j = 0; j < strlen( licensePlate ); j++ ) {
+            if ( licensePlate[j] != ' ' && isdigit ( licensePlate[j] ) == 0 ) {
                     
-                for (int i2 = 0; i2 < strlen(words[i]); i2++) {
+                for ( int i2 = 0; i2 < strlen( words[i] ); i2++ ) {
                     /*
-                    printf("%c = %c ( %s ) : %d\n",
+                    printf( "%c = %c ( %s ) : %d\n",
                         licensePlate[j], words[i][i2], wordsCopy[i],
-                        toupper(licensePlate[j]) == toupper(wordsCopy[i][i2]));
+                        toupper( licensePlate[j] ) == toupper( wordsCopy[i][i2] ) );
                     */
                         
-                    if (toupper(licensePlate[j]) == toupper(wordsCopy[i][i2])) {
+                    if ( toupper( licensePlate[j] ) == toupper( wordsCopy[i][i2] ) ) {
                         wordsCopy[i][i2] = ' ';
                         c++;
                         change = 1;
@@ -51,16 +58,16 @@ char * shortestCompletingWord(char * licensePlate, char ** words, int wordsSize)
             }
         }
         
-        if (c == strlen(licensePlate)) {
-            if (strlen(words[i]) < shortSize || shortSize == -1) {
-                shortSize = strlen(words[i]);
+        if ( c == strlen( licensePlate ) ) {
+            if ( strlen( words[i] ) < shortSize || shortSize == -1 ) {
+                shortSize = strlen( words[i] );
                 shortestWord = i;
             }
         }
 
     }
 
-    printf("%d ", shortSize);
-    printf("%s", words[shortestWord]);
+    printf( "%d ", shortSize );
+    printf( "%s", words[shortestWord] );
     return words[shortestWord];
 }
